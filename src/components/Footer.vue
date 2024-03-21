5825efaabb06a4cba61294ac2a9438793898ce8e
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-03-21 17:50:11 +0100

    [add] variables to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,34 @@
 </script>
 
 <template>
-    
+    <footer>
+        <section class="container d-flex">
+            <div>
+                <p>Boolando s.r.l</p>
+                <div class="d-flex">
+                    <span>Informazioni legali</span>
+                    <span>Normativa sulla privacy</span>
+                    <span>Diritto di recesso</span>
+                </div>
+            </div>
+            <div class="social">
+                <p>Trovaci anche su</p>
+                <a href="#"><i class="fa-brands fa-square-x-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-square-pinterest"></i></a>
+                <a href="#"><i class="fa-brands fa-square-youtube"></i></a>
+            </div>
+        </section>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
 
+footer{
+    background: $bg-footer;
+    color: white;
+    padding: 20px 0;
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-    
+    <div>
+        <h1>Main</h1>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-
+    h1{
+        font-size: 5em;
+        text-align: center;
+        margin-top: 3em;
+    }
 </style>
