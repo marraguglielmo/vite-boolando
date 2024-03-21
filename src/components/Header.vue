9af63b36ab4dd2326eb26a78ef0f4d7f1a8a31a4
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-03-21 16:10:22 +0100

    [add] variabili, general scss

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,36 @@
 </script>
 
 <template>
-    <div>
-        <h2>Header</h2>
-    </div>
+    <header>
+        <section class="container d-flex ">
+            <!-- TOPBAR -->
+            <nav>
+                <ul class="categories d-flex ">
+                    <li><a href="#">Donna</a></li>
+                    <li><a href="#">Uomo</a></li>
+                    <li><a href="#">Bambini</a></li>
+                </ul>
+            </nav>
+            <!-- /TOPBAR -->
+            <!-- LOGO -->
+            <span class="logo ">
+                <p>logo</p>
+                <!-- <img src="img/boolean-logo.png" alt="logo"> -->
+            </span>
+            <!-- /LOGO -->
+            <!-- MENU -->
+            <span>
+                <nav>
+                    <ul class="menu d-flex">
+                        <li><a href="#"><i class="fa-regular fa-user"></i></a></li>
+                        <li><a href="#"><i class="fa-regular fa-heart"></i></a></li>
+                        <li><a href="#"><i class="fa-solid fa-bag-shopping"></i></a></li>
+                    </ul>
+                </nav>
+            </span>
+            <!-- /MENU -->
+        </section>
+    </header>
 </template>
 
 <style lang="scss" scoped>
